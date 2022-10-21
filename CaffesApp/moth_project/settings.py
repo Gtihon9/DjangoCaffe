068c1ca9e32d50484ae84088196921894016e691
Gtihon9 <gtihon9@gmail.com>
2022-10-21 17:45:32 +0300

    Password reset backend(filebased)

--- CaffesApp/moth_project/settings.py
+++ CaffesApp/moth_project/settings.py
@@ -154,3 +154,5 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT')
 USE_TLS = config('USE_TLS', cast=bool, default=True)
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
