7bc2f266f23cf09b2c98040cbe4658346dbe87f9
Gtihon9 <gtihon9@gmail.com>
2022-11-01 15:11:08 +0300

    logout redirect ro current page

--- CaffesApp/caffes/urls.py
+++ CaffesApp/caffes/urls.py
@@ -5,9 +5,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.detail_view, name='detail'),
-    path('<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
-    path('<int:caffe_pk>/comment/<int:comment_pk>/remove/',
+    path('caffe/<int:pk>/', views.detail_view, name='detail'),
+    path('caffe/<int:pk>/add_comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('caffe/<int:caffe_pk>/comment/<int:comment_pk>/remove/',
          views.comment_remove, name='comment_remove'),
-    path('<int:caffe_pk>/comment/<int:comment_pk>/edit/', views.comment_edit, name='comment_edit'),
+    path('caffe/<int:caffe_pk>/comment/<int:comment_pk>/edit/', views.comment_edit, name='comment_edit'),
     ]
